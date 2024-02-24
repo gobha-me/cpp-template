e53c75cfb2c7ef118e955838310c5cb2c7b48152
Jeffrey Smith <jasafpro@gmail.com>
2024-02-24 12:39:09 -0500

    auto versioning based on nearest tag, there maybe some improvements needed, but a good start

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -8,19 +8,26 @@ find_package (Git)
 if (GIT_FOUND)
   # Retrieve nearest tag
   execute_process(
-    COMMAND ${GIT_EXECUTABLE} describe --tags
+    COMMAND ${GIT_EXECUTABLE} describe --tags --dirty
     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
     RESULT_VARIABLE GIT_RESULT
     OUTPUT_VARIABLE GIT_TAG_STR
   )
-  # AM I a tag
-  #   Then what is my name and does it match expression \d+(\.\d+){0,3}
-  #     Then VERSION = tag
-  # else if am I a branch from a tag
-  #   Then VERSION = tag.<build>
-  # else if am I a branch from main/master
-  #   Then VERSION = <next potential tag>? Should be a rule, bump to major, or minor
-  #    May not matter as this branch is not a tagged release
+
+  if (GIT_RESULT AND NOT GIT_RESULT EQUAL 0)
+    message(STATUS "No tags found")
+  else()
+    string(STRIP ${GIT_TAG_STR} GIT_TAG_STR)
+    string(REGEX MATCH "^[rv]?([0-9]+(\.[0-9]+)+)(-.+)?$" CURRENT_VERSION "${GIT_TAG_STR}")
+
+    if (CMAKE_MATCH_1)
+      set(VERSION ${CMAKE_MATCH_1})
+    endif()
+
+    if (CMAKE_MATCH_3) # Use inplace of tweak?
+      set(DIRTY_BRANCH ${CMAKE_MATCH_1})
+    endif()
+  endif()
 endif (GIT_FOUND)
 
 # I am no longer using subversion, having for a more than a few years now
@@ -28,4 +35,6 @@ endif (GIT_FOUND)
 #   moved.  With that said, git is not the only tool for this.  Should I even bother
 #   Supporting anything else?
 
-set(VERSION 0.0.0.1)
+if (NOT VERSION)
+  set(VERSION 0.0.0.1)
+endif()
